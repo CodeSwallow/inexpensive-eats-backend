8e115bf5b5ab0a3a8b39a73b24cf4769f55e750e
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-10-25 17:59:37 -0600

    feat: created restaurant model

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    price_level = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    rating = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    website = models.URLField(null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
