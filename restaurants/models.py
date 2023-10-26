f8bbe669d44a77d002f392e9ed5f23ed88e08c9c
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-10-25 21:35:52 -0600

    feat: added complementary models

--- restaurants/models.py
+++ restaurants/models.py
@@ -5,8 +5,8 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
     price_level = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     rating = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(5)])
     website = models.URLField(null=True, blank=True)
@@ -17,3 +17,32 @@ class Restaurant(models.Model):
     def __str__(self):
         return self.name
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    restaurants = models.ManyToManyField(Restaurant, related_name='categories')
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    review_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.rating} - {self.review_text[:50]}'
+
+
+class Image(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='restaurant_images/')
+    description = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.restaurant.name} - {self.description}'
