fcc4c932a34d3dab1046a4b5bb473c56c6f1647d
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-11-03 23:32:07 -0600

    feat: created home view page

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from restaurants.models import Restaurant
+
+
+class HomePageView(View):
+    template_name = 'restaurants/home.html'
+
+    def get(self, request, *args, **kwargs):
+        newest_restaurants = Restaurant.objects.order_by('-created_at')[:5]
+        popular_restaurants = Restaurant.objects.order_by('-rating')[:5]
+
+        return render(request, self.template_name, {
+            'newest_restaurants': newest_restaurants,
+            'popular_restaurants': popular_restaurants
+        })
