6f7e649ab81fa1245f865fca6877ea56b6a0438e
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-11-03 22:47:00 -0600

    feat: updated spider and scraper

--- restaurants/management/commands/scrape_restaurants.py
+++ restaurants/management/commands/scrape_restaurants.py
@@ -1,11 +1,46 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 
-from restaurants.models import Restaurant
+from restaurants.models import Restaurant, PriceLevel, Category
+from restaurants.spiders.restaurant_spider import RestaurantSpider
 
 
 class Command(BaseCommand):
-    help = 'Scrapes restaurants from Google Places API'
+    help = 'Scrapes restaurants from site and saves them to the database'
 
     def handle(self, *args, **options):
-        Restaurant.objects.all().delete()
-        self.stdout.write(self.style.SUCCESS('Successfully deleted all restaurants'))
+        process = CrawlerProcess(get_project_settings())
+        process.crawl(RestaurantSpider)
+        process.start()
+
+    def process_item(self, item):
+        price_level = self.get_or_create_price_level(item['price_range'])
+        category, _ = Category.objects.get_or_create(name=item['category_name'])
+        restaurant, created = Restaurant.objects.get_or_create(
+            name=item['name'],
+            defaults={
+                'price_level': price_level,
+                'description': item.get('description', ''),
+            }
+        )
+        restaurant.categories.add(category)
+        if not created:
+            restaurant.price_level = price_level
+            restaurant.description = item.get('description', '')
+            restaurant.save()
+
+    def get_or_create_price_level(self, price_range):
+        try:
+            average_price = Decimal(price_range.replace('mxn', '').strip())
+        except (ValueError, DecimalException):
+            self.stderr.write(self.style.ERROR(f'Invalid price range format: {price_range}'))
+            return None
+
+        price_levels = PriceLevel.objects.all()
+        for price_level in price_levels:
+            if price_level.min_price <= average_price <= price_level.max_price:
+                return price_level
+
+        self.stderr.write(self.style.WARNING(f'No price level found for average price {average_price}'))
+        return None
--- restaurants/spiders/retaurant_spider.py
+++ restaurants/spiders/retaurant_spider.py
@@ -1,64 +1,31 @@
+import os
+
 import scrapy
 
 from decimal import Decimal
 
-from django.db import transaction
-
-from restaurants.models import Restaurant, PriceLevel
-
 
 class RestaurantSpider(scrapy.Spider):
     name = 'restaurant_spider'
-    allowed_domains = ['zonaturistica.com']
-    start_urls = ['https://www.zonaturistica.com/restaurantes/puebla/puebla']
+    allowed_domains = os.getenv('RESTAURANT_SPIDER_ALLOWED_DOMAINS').split(',')
+    start_urls = os.getenv('RESTAURANT_SPIDER_URLS').split(',')
 
     def parse(self, response, **kwargs):
-        price_levels = PriceLevel.objects.all()
         restaurant_containers = response.css('div[id^="item-restaurante-"]')
 
         for restaurant_selector in restaurant_containers:
-            name = restaurant_selector.css('div.nombre-restaurante a::text').get()
+            name = restaurant_selector.css('div.nombre-restaurante a::text').get(default='').strip()
+            price_range = restaurant_selector.css('span[itemprop="priceRange"]::text').get(default='').strip()
+            category_name = restaurant_selector.css('div.txt100[itemprop="servesCuisine"]::text').get(
+                default='').strip()
+            description = restaurant_selector.css('p.descripcion-restaurante::text').get(default='').strip()
 
-            if not name:
-                self.log('No name found for restaurant')
-                continue
-
-            price_range = restaurant_selector.css('span[itemprop="priceRange"]::text').get()
-            category_name = restaurant_selector.css('div.txt100[itemprop="servesCuisine"]::text').get()
-            description = restaurant_selector.css('p.descripcion-restaurante::text').get()
-
-            if price_range:
-                average_price = Decimal(price_range.replace('mxn', '').strip())
-                price_level = self.get_price_level(average_price, price_levels)
+            if name and price_range and category_name:
+                yield {
+                    'name': name,
+                    'price_range': price_range,
+                    'category_name': category_name,
+                    'description': description,
+                }
             else:
-                self.log(f'No price range found for {name}')
-                continue
-
-            if not category_name:
-                self.log(f'No category name found for {name}')
-                continue
-
-            category, _ = Category.objects.get_or_create(name=category_name)
-
-            with transaction.atomic():
-                restaurant, created = Restaurant.objects.get_or_create(
-                    name=name,
-                    defaults={
-                        'price_level': price_level,
-                        'description': description
-                    }
-                )
-
-                restaurant.categories.add(category)
-
-                if not created:
-                    restaurant.price_level = price_level
-                    restaurant.description = description
-                    restaurant.save()
-
-    def get_price_level(self, average_price, price_levels):
-        for price_level in price_levels:
-            if price_level.min_price <= average_price <= price_level.max_price:
-                return price_level
-        self.log(f'No price level found for average price {average_price}')
-        return None
+                self.log(f'Missing data in restaurant: {name or "Unknown"}, URL: {response.url}')
